96dd52cdb9b6706121e25bd41520607cae5c98bf
Joseph <munemojoseph332@gmail.com>
2025-08-07 23:03:26 +0200

    removed authentication

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -4,5 +4,5 @@ from fastapi import HTTPException
 def verify_google_token(token:str):
     response = requests.get(f"https://oauth2.googleapis.com/tokeninfo?id_token={token}")
     if response.status_code != 20:
-        raise HTTPException(status_code=401,detail="Invalid Google token")
-    return response.json()
+        #raise HTTPException(status_code=401,detail="Invalid Google token")
+        return response.json()
